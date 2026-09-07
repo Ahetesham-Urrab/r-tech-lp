948812bd41a90970e30aa0bda5b32bfa9c09e97b
naik-aftab <aftabnaik1999@gmail.com>
2026-09-07 13:59:24 +0530

    Update website

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
         {/* Google Tag Manager */}
         <Script
           id="gtm-script"
@@ -31,25 +30,26 @@ export default function RootLayout({
               w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
               var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
               f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PH5QDC9X');
+              })(window,document,'script','dataLayer','GTM-5RVTLSRK');
             `,
           }}
         />
-
       </head>
 
       <body className={`site-bg text-gray-900 ${googleSansLike.className}`}>
-
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PH5QDC9X"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5RVTLSRK"
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }}
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
           />
         </noscript>
 
@@ -61,7 +61,6 @@ export default function RootLayout({
 
         {/* Lead Popup */}
         <LeadPopupController />
-
       </body>
     </html>
   );
